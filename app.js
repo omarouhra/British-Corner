16facf9b9021389748bbbb38a76ed246c709602b
Omar Ouhra <omarouhra@Omars-MacBook-Pro.local>
2021-01-04 14:03:48 +0100

    scroll js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const navSlide = () => {
+  // nav burger
   const burger = document.querySelector('.burger')
   const nav = document.querySelector('.nav-links')
   const navLinks = document.querySelectorAll('.nav-links li')
+
+  // services toggle
+  const btn1 = document.querySelector('#button1')
+  const btn2 = document.querySelector('#button2')
+  const btn3 = document.querySelector('#button3')
+  const btn4 = document.querySelector('#button4')
+
+  const serviceTitle = document.querySelector('.services_title h3')
+  const firstText = document.querySelector('.first')
+  const secondText = document.querySelector('.second')
+  const thirdText = document.querySelector('.third')
+  const fourthText = document.querySelector('.fourth')
 
   //  Toogle nav
   burger.addEventListener('click', () => {
@@ -19,6 +32,36 @@ const navSlide = () => {
     })
     //   Burger animation
     burger.classList.toggle('is-active')
+  })
+
+  // service 1
+  btn1.addEventListener('click', () => {
+    if (firstText.style.display) {
+      // serviceTitle.style.fontWeight = ''
+      firstText.style.display = ''
+      btn1.style.transform = ''
+    } else {
+      // secondText.style.display = 'none'
+      // thirdText.style.display = 'none'
+      // fourthText.style.display = 'none'
+      firstText.style.display = 'block'
+      firstText.style.opacity = 1
+      btn1.style.transform = 'rotate(90deg)'
+      // serviceTitle.style.fontWeight = '900'
+    }
+  })
+  // service 2
+  btn2.addEventListener('click', () => {
+    if (secondText.style.display) {
+      // serviceTitle.style.fontWeight = ''
+      secondText.style.display = ''
+      btn2.style.transform = ''
+    } else {
+      secondText.style.display = 'block'
+      secondText.style.opacity = 1
+      btn2.style.transform = 'rotate(90deg)'
+      // serviceTitle.style.fontWeight = '900'
+    }
   })
 }
 
